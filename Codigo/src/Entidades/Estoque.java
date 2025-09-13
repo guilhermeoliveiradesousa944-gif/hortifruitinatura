116353bd7db41dc532bf1e157a25865b08e0b2ba
Felipe_Galdino <felipebatistagaldino.pessoal@gmail.com>
2025-09-13 11:12:09 -0300

    Atualizado

--- Codigo/src/Entidades/Estoque.java
+++ Codigo/src/Entidades/Estoque.java
@@ -7,4 +7,34 @@ public class Estoque {
     private int quantidadeAtual, quantidadeMinima;
     private Date dataUltimaAtualizacao;
 
+    //gets e sets
+    public int getQuantidadeAtual() {
+        return quantidadeAtual;
+    }
+    public void setQuantidadeAtual(int quantidadeAtual) {
+        this.quantidadeAtual = quantidadeAtual;
+    }
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+    public Date getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
+    }
+    public void setDataUltimaAtualizacao(Date dataUltimaAtualizacao) {
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+    }
+
+    public void AtulizarEstoque(){
+
+    }
+    public void ExcluirItem(){
+
+    }
+    public void CadastarProduto(){
+        
+    }
+
 }
--- Codigo/src/Entidades/ItenVenda.java
+++ Codigo/src/Entidades/ItenVenda.java
@@ -4,4 +4,26 @@ public class ItenVenda {
     //atributos
     private int quantidade, atribute55;
     private double precoUnitario;
+
+    //gets e sets
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public int getAtribute55() {
+        return atribute55;
+    }
+    public void setAtribute55(int atribute55) {
+        this.atribute55 = atribute55;
+    }
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    
 }
